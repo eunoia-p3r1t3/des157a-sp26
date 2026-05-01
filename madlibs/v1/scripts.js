e3098ea642e719a6b757648e3431fb039a8a2429
Elsa Cespedes <elsa.moonchild@gmail.com>
2026-04-30 22:42:10 -0700

    final V1

--- madlibs/v1/scripts.js
+++ madlibs/v1/scripts.js
@@ -32,25 +32,36 @@
             makeMadlib(words);
         }
     }
-    const errorId ='';
-    const errorText = `Please fill out this field ${errorId}`;
+    
 
     function showErrors(formData, emptyfields) {
         const errorId = formData[emptyfields[0]].id;
+        const errorText = `Please fill out this field ${errorId}`;
         myMadlib.innerHTML = errorText;
-        document.querySelector (`#$errorId`).focus();   
+        document.querySelector (`#${errorId}`).focus();   
     }
 
     function makeMadlib(words){
-        const myTextOne = `<p>Hello there ${words[0]} Humans,</p>
+        const myTextOne = `<div id="madlib">
+                                <div class="image2"><img class="alien" src="images/alien.png" alt="illustrated alien"></div>    
+                                <section class="letter">
+                                
+                                <p>Hello there ${words[0]} Humans,</p>
 
-        <p>We come from the far away planet of ${words[1]}. We aliens have come ${words[2]} to your ${words[3]} planet and hope to ${words[4]} become a part of your world, and pose as ${words[5]} in order to learn more about your society. We really want to use this information in order to ${words[6]} and make our home planet much more ${words[7]}. Our reasoning for this is that ${words[8]} years ago our planet was overrun by ${words[9]} ${words[10]}, and made all of our ${words[11]}, lose their ${words[12]}. We really hope that you can understand and accept us during our visit.</p>
+                                <p>We come from the far away planet of ${words[1]}. We aliens have come ${words[2]} to your ${words[3]} planet and hope to ${words[4]} become a part of your world, and pose as ${words[5]} in order to learn more about your society. We really want to use this information in order to ${words[6]} and make our home planet much more ${words[7]}. Our reasoning for this is that ${words[8]} years ago our planet was overrun by ${words[9]} ${words[10]}, and made all of our ${words[11]}, lose their ${words[12]}. We really hope that you can understand and accept us during our visit.</p>
         
-        <p>Wishing you the best,</p>
-        <p>Your friendly galactic Alien</p>`;
+                                <p>Wishing you the best,</p>
+                                <p>Your friendly galactic Alien</p>`;
+        
+        document.querySelector('#madlib-outputs').style.display ="block";
+        document.querySelector('#madlib-questions').style.display ="none";
+        
         myMadlib.innerHTML = myTextOne;
+        
         for(const eachField of formData){
             eachField.value = '';
         }
+        
     }
+
 } )();
