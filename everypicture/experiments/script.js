04115d4086ce10f95e4f59188d80d65317625d51
Elsa Cespedes <elsa.moonchild@gmail.com>
2026-05-14 09:31:47 -0700

    update

---
+++ everypicture/experiments/script.js
@@ -0,0 +1,56 @@
+(function(){
+    'use strict'
+    console.log('reading js')
+    document.querySelector('#object1').addEventListener('pointerenter', function(){
+        console.log('over object 1');
+    });
+    document.querySelector('#object2').addEventListener('pointerenter', function(){
+        console.log('over object 2');
+    });
+    document.querySelector('#object3').addEventListener('pointerenter', function(){
+        console.log('over object 3');
+    });
+    document.querySelector('#object4').addEventListener('pointerenter', function(){
+        console.log('over object 4');
+    });
+    document.querySelector('#object5').addEventListener('pointerenter', function(){
+        console.log('over object 5');
+    });
+    document.querySelector('#object6').addEventListener('pointerenter', function(){
+        console.log('over object 6');
+    });
+    document.querySelector('#object7').addEventListener('pointerenter', function(){
+        console.log('over object 7');
+    });
+    document.querySelector('#object8').addEventListener('pointerenter', function(){
+        console.log('over object 8');
+    });
+    document.querySelector('#object9').addEventListener('pointerenter', function(){
+        console.log('over object 9');
+    });
+    document.querySelector('#object10').addEventListener('pointerenter', function(){
+        console.log('over object 10');
+    });
+    document.querySelector('#object11').addEventListener('pointerenter', function(){
+        console.log('over object 11');
+    });
+    document.querySelector('#object12').addEventListener('pointerenter', function(){
+        console.log('over object 12');
+    });
+    document.querySelector('#object13').addEventListener('pointerenter', function(){
+        console.log('over object 13');
+    });
+    document.querySelector('#object14').addEventListener('pointerenter', function(){
+        console.log('over object 14');
+    });
+    document.querySelector('#object15').addEventListener('pointerenter', function(){
+        console.log('over object 15');
+    });
+    document.querySelector('#object16').addEventListener('pointerenter', function(){
+        console.log('over object 16');
+    });
+    document.querySelector('#object17').addEventListener('pointerenter', function(){
+        console.log('over object 17');
+    });
+    
+})();
