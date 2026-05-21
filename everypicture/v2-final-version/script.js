fc342e7347be85f22f3b7fcc0ddba0a67b3a35bb
Elsa Cespedes <elsa.moonchild@gmail.com>
2026-05-21 16:20:48 -0700

    update

--- everypicture/v2-final-version/script.js
+++ everypicture/v2-final-version/script.js
@@ -1,56 +1,47 @@
 (function(){
     'use strict'
     console.log('reading js')
-    document.querySelector('#object1').addEventListener('pointerenter', function(){
-        console.log('over object 1');
-    });
-    document.querySelector('#object2').addEventListener('pointerenter', function(){
-        console.log('over object 2');
-    });
-    document.querySelector('#object3').addEventListener('pointerenter', function(){
-        console.log('over object 3');
-    });
-    document.querySelector('#object4').addEventListener('pointerenter', function(){
-        console.log('over object 4');
-    });
-    document.querySelector('#object5').addEventListener('pointerenter', function(){
-        console.log('over object 5');
-    });
-    document.querySelector('#object6').addEventListener('pointerenter', function(){
-        console.log('over object 6');
-    });
-    document.querySelector('#object7').addEventListener('pointerenter', function(){
-        console.log('over object 7');
-    });
-    document.querySelector('#object8').addEventListener('pointerenter', function(){
-        console.log('over object 8');
-    });
-    document.querySelector('#object9').addEventListener('pointerenter', function(){
-        console.log('over object 9');
-    });
-    document.querySelector('#object10').addEventListener('pointerenter', function(){
-        console.log('over object 10');
-    });
-    document.querySelector('#object11').addEventListener('pointerenter', function(){
-        console.log('over object 11');
-    });
-    document.querySelector('#object12').addEventListener('pointerenter', function(){
-        console.log('over object 12');
-    });
-    document.querySelector('#object13').addEventListener('pointerenter', function(){
-        console.log('over object 13');
-    });
-    document.querySelector('#object14').addEventListener('pointerenter', function(){
-        console.log('over object 14');
-    });
-    document.querySelector('#object15').addEventListener('pointerenter', function(){
-        console.log('over object 15');
-    });
-    document.querySelector('#object16').addEventListener('pointerenter', function(){
-        console.log('over object 16');
-    });
-    document.querySelector('#object17').addEventListener('pointerenter', function(){
-        console.log('over object 17');
-    });
+
+    const myBag= document.querySelector('#mainpage');
     
+        myBag.addEventListener('click', function(){
+            const lookInside = document.querySelector('#bag');
+
+        document.querySelector('#mainpage').style.display ="none";
+        
+        document.querySelector('#inner').style.display ="block";
+        });
+
+        const myDiv = document.querySelector('#new');
+        const air =document.querySelector('#pods');
+
+        myDiv.addEventListener('click', function(){
+            myDiv.innerHTML= '<img id="pods" src="images/airpods.png" alt="whats in my bag"><img id="rockout" src="images/letsrock.png" alt="whats in my bag">';
+        });
+        air.addEventListener('click',function(){
+            air.innerHTML= '<img id="pods" src="images/airpods.png" alt="whats in my bag"><img ';
+        });
+        
+        
+        document.querySelector('#emer').addEventListener('click', function(event) {
+            event.preventDefault();
+            document.querySelector('#overlay').className = 'showing';
+        });
+        document.querySelector('.close').addEventListener('click', function(event) {
+            event.preventDefault();
+            document.querySelector('#overlay').className = 'hidden';
+        });
+
+        document.addEventListener('keydown', function(event){
+            if( event.key === "Escape"){
+            document.querySelector('#overlay').className = 'hidden';
+            }
+        });
+        const outline = document.querySelector('#outline')
+        outline.addEventListener('mouseover',function(){
+            outline.style.opacity = '100%';
+        });
+        outline.addEventListener('mouseout',function(){
+            outline.style.opacity = '0';
+        });
 })();
